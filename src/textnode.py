ea3ade97478c60a57974cc6aefb3c81aebe47ec4
Tyler Spear <tylerspear@Tylers-iMac.local>
2025-01-06 19:09:12 -0600

    text_node_to_html_node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     NORMAL = "Normal"
@@ -21,5 +22,28 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("Text type not valid")
+    
+        if TextType.NORMAL:
+            return LeafNode(None, text_node.text)
+        if TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        if TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        if TextType.LINK:
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        if TextType.IMAGE:
+            return LeafNode("img", None, {"src":text_node.url, "alt":text_node.text})
+        if TextType.CODE:
+            return LeafNode("code", text_node.text)
+
+
+
+        
+
+
 
 
