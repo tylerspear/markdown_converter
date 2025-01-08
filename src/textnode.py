b922bd4cd77944770e2fac8e328c9ee4f68a17ef
Tyler Spear <tylerspear@Tylers-iMac.local>
2025-01-07 20:36:15 -0600

    inline text delimiter splitting

--- src/textnode.py
+++ src/textnode.py
@@ -26,19 +26,22 @@ class TextNode:
 def text_node_to_html_node(text_node):
     if text_node.text_type not in TextType:
         raise Exception("Text type not valid")
-    
-        if TextType.NORMAL:
+
+    match text_node.text_type:
+        case TextType.NORMAL:
             return LeafNode(None, text_node.text)
-        if TextType.BOLD:
+        case TextType.BOLD:
             return LeafNode("b", text_node.text)
-        if TextType.ITALIC:
+        case TextType.ITALIC:
             return LeafNode("i", text_node.text)
-        if TextType.LINK:
+        case TextType.LINK:
             return LeafNode("a", text_node.text, {"href":text_node.url})
-        if TextType.IMAGE:
-            return LeafNode("img", None, {"src":text_node.url, "alt":text_node.text})
-        if TextType.CODE:
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
+        case TextType.CODE:
             return LeafNode("code", text_node.text)
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
 
 
 
